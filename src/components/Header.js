a1b59b3d0eaf317f2475c3606698697603b46ce8
Dexdot <dexdotapb@gmail.com>
2018-07-22 00:04:42 +0300

    Reorganizing routes

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to="/" exact activeClassName="is-active">
+      Home
+    </NavLink>
+    <NavLink to="/create" exact activeClassName="is-active">
+      Create expense
+    </NavLink>
+    <NavLink to="/edit" exact activeClassName="is-active">
+      Edit expense
+    </NavLink>
+    <NavLink to="/help" exact activeClassName="is-active">
+      Help
+    </NavLink>
+  </header>
+);
+
+export default Header;
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Header from '../components/Header';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+import AddExpensePage from '../components/AddExpensePage';
+import EditExpensePage from '../components/EditExpensePage';
+import HelpPage from '../components/HelpPage';
+import NotFoundPage from '../components/NotFoundPage';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const AppRouter = (
+  <Router>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact />
+        <Route path="/create" component={AddExpensePage} exact />
+        <Route path="/edit" component={EditExpensePage} exact />
+        <Route path="/help" component={HelpPage} exact />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default AppRouter;
