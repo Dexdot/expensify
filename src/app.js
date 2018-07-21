5d6b8c2125f0d792e66bfdd1949cd95a348f69e9
Dexdot <dexdotapb@gmail.com>
2018-07-21 23:33:22 +0300

    Router basics. 404

--- src/app.js
+++ src/app.js
@@ -1,7 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import 'normalize.css/normalize.css';
 import './styles/styles.sass';
 
-ReactDOM.render(<p>Boilerplate</p>, document.querySelector('#app'));
+const ExpenseDashboardPage = () => (
+  <div>This is from my dashboard component</div>
+);
+const AddExpensePage = () => <div>This is from my add component</div>;
+const EditExpensePage = () => <div>This is from my edit component</div>;
+const HelpPage = () => <div>This is from my help component</div>;
+const NotFoundPage = () => <div>404!</div>;
+
+const routes = (
+  <Router>
+    <Switch>
+      <Route path="/" component={ExpenseDashboardPage} exact />
+      <Route path="/create" component={AddExpensePage} exact />
+      <Route path="/edit" component={EditExpensePage} exact />
+      <Route path="/help" component={HelpPage} exact />
+      <Route component={NotFoundPage} />
+    </Switch>
+  </Router>
+);
+
+ReactDOM.render(routes, document.querySelector('#app'));
