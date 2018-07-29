d51d2d4fb5419ca1ac12cb592cbca62cabe77659
Dexdot <dexdotapb@gmail.com>
2018-07-29 16:51:54 +0300

    Text input filter and remove expense button

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-const Expense = ({ description, amount, createdAt }) => (
+const Expense = ({ id, description, amount, createdAt, dispatch }) => (
   <div>
     <h3>{description}</h3>
     <p>
       {amount} - {createdAt}
     </p>
-    <br />
+    <button
+      onClick={() => {
+        dispatch(removeExpense({ id }));
+      }}
+    >
+      Remove
+    </button>
   </div>
 );
 
-export default Expense;
+const mapStateToProps = state => {
+  expenses: state.expenses;
+};
+
+export default connect(mapStateToProps)(Expense);
