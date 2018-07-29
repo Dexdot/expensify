6f725babd7b1084b6d2dcd328329ab5c5d3f4c89
Dexdot <dexdotapb@gmail.com>
2018-07-29 17:06:35 +0300

    Sorting dropdown

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 
 import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
 
 import getVisibleExpenses from './selectors/expenses';
 
@@ -17,9 +16,12 @@ import './styles/styles.sass';
 // Store
 const store = configureStore();
 
+// Dispatching
 store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 300 }));
-store.dispatch(setTextFilter('bill'));
+store.dispatch(
+  addExpense({ description: 'Gas bill', amount: 300, createdAt: 1000 })
+);
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
 
 // Render
 const jsx = (
